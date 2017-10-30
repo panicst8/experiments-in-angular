92e822440cad58e29c5b29445ec7b163f4057631
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-10-30 17:15:52 -0500

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './shared/modules/material/material.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent],
+   entryComponents: [],
+   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+   providers: [],
+   bootstrap: [AppComponent],
+   exports: [MaterialModule],
 })
-export class AppModule { }
+export class AppModule {}
