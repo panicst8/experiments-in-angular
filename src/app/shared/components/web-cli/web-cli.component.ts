df5b5763fc574c8ab06dd4080757b5965d6cb840
Chris Milford <panicst8@yahoo.com>
2018-07-01 01:33:54 -0500

    still...

--- src/app/shared/components/web-cli/web-cli.component.ts
+++ src/app/shared/components/web-cli/web-cli.component.ts
@@ -16,26 +16,62 @@ export class WebCliComponent implements OnInit {
   @ViewChild('cmdPrompt') cmdPromptElement: ElementRef;
   @ViewChild('cmdPromptOutputDiv') cmdPromptOutputDivElement: ElementRef;
   @Input() consoleActive;
-
-  // This is what allows parent to focus input -- prob see if hide show can trigger this
   @Input()
   set setProp(p: boolean) {
     console.log(p);
     setTimeout(() => {
-      this.focus();
+      this.cmdPromptControls('focus');
     }, 0);
   }
 
+  private history = [];
+  public cmdOffSet = (function() {
+    let offset = 0;
+    return function(adjustment = 0) {
+      if (adjustment === -42) {
+        offset = 0;
+      } else {
+        offset += adjustment;
+      }
+      return offset;
+    };
+  })();
+
   constructor(private el: ElementRef, private _renderer: Renderer2) {}
 
-  ngOnInit() {}
-
-  executeCommand(event) {
-    this.writeLine(event.target.value);
+  ngOnInit() {
+    this.showGreeting();
   }
 
-  focus() {
-    this.cmdPromptElement.nativeElement.focus();
+  executeCommand(event) {
+    const command = event.target.value.trim();
+    // this.writeHTML('<p>Boom!</p>');
+    this.cmdOffSet(-42);
+    this.cmdPromptControls('clear');
+    this.writeLine(command, 'cmd');
+    if (!command) {
+      return;
+    }
+    this.history.push(command);
+  }
+
+  browseHistory(adjustment) {
+    this.cmdOffSet(adjustment);
+    if (this.cmdOffSet() === 5) {
+      this.cmdOffSet();
+    }
+    console.log(this.cmdOffSet());
+  }
+
+  cmdPromptControls(action) {
+    switch (action) {
+      case 'focus':
+        this.cmdPromptElement.nativeElement.focus();
+        break;
+      case 'clear':
+        this.cmdPromptElement.nativeElement.value = '';
+        break;
+    }
   }
 
   scrollToBottom() {
@@ -52,6 +88,18 @@ export class WebCliComponent implements OnInit {
     span.className = `webCli-${cssSuffix}`;
     span.innerText = txt;
     this.cmdPromptOutputDivElement.nativeElement.appendChild(span);
+    this.newLine();
+  }
+
+  writeHTML(markUp) {
+    const div = document.createElement('div');
+    div.innerHTML = markUp;
+    this.cmdPromptOutputDivElement.nativeElement.appendChild(div);
+    this.newLine();
+  }
+
+  showGreeting() {
+    this.writeLine('Web CLI [Version 0.0.1]', 'cmd');
     this.newLine();
   }
 }
