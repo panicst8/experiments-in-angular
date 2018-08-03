e63cccef5f760e1f56f7402af1aad22161602e39
Chris Milford <panicst8@yahoo.com>
2018-08-02 23:16:43 -0500

    workingish

--- src/app/shared/components/web-cli/web-cli.component.ts
+++ src/app/shared/components/web-cli/web-cli.component.ts
@@ -1,10 +1,10 @@
 import {
   Component,
-  OnInit,
-  ViewChild,
-  Input,
   ElementRef,
+  Input,
+  OnInit,
   Renderer2,
+  ViewChild,
 } from '@angular/core';
 
 @Component({
@@ -15,6 +15,7 @@ import {
 export class WebCliComponent implements OnInit {
   @ViewChild('cmdPrompt') cmdPromptElement: ElementRef;
   @ViewChild('cmdPromptOutputDiv') cmdPromptOutputDivElement: ElementRef;
+  @ViewChild('fakeInput') fakeInput: ElementRef;
   @Input() consoleActive;
   @Input()
   set setProp(p: boolean) {
@@ -48,8 +49,61 @@ export class WebCliComponent implements OnInit {
     this.showGreeting();
   }
 
-  readyCommand(event) {
-    const command = event.target.value.trim();
+  otherKey(keyPress: KeyboardEvent) {
+    console.log(keyPress);
+    if (keyPress.key === 'ArrowUp') {
+      this.browseHistory(+1);
+      console.log('up up');
+    } else if (keyPress.key === 'ArrowDown') {
+      console.log('down down');
+      this.browseHistory(-1);
+    } else {
+      console.log(keyPress.key);
+    }
+    //  (keyup.arrowup)="browseHistory(+1)" (keyup.arrowdown)="browseHistory(-1);"
+    //   (keyup.enter)="readyCommand($event)">
+    if (
+      keyPress.key === 'Alt' ||
+      keyPress.key === 'Control' ||
+      keyPress.altKey ||
+      keyPress.ctrlKey
+    ) {
+      return false;
+    }
+
+    if (keyPress.key === 'Enter') {
+      this.readyCommand(this.fakeInput.nativeElement.innerText);
+      console.log('Try Command: ', this.fakeInput.nativeElement.innerText);
+      this.fakeInput.nativeElement.innerText = '';
+    } else if (keyPress.key === 'Backspace') {
+      this.fakeInput.nativeElement.innerText = this.fakeInput.nativeElement.innerText.slice(
+        0,
+        -1
+      );
+    } else if (this.validKey(keyPress.keyCode)) {
+      this.fakeInput.nativeElement.innerText =
+        this.fakeInput.nativeElement.innerText + keyPress.key;
+      console.log('should add: ', keyPress, keyPress.key);
+    } else {
+      console.log('other key: ', keyPress, keyPress.key);
+    }
+  }
+
+  validKey(keyCode: number) {
+    const isValid =
+      (keyCode > 47 && keyCode < 58) || // number keys
+      keyCode === 32 ||
+      keyCode === 13 || // spacebar & return key(s) (if you want to allow carriage returns)
+      (keyCode > 64 && keyCode < 91) || // letter keys
+      (keyCode > 95 && keyCode < 112) || // numpad keys
+      (keyCode > 185 && keyCode < 193) || // ;=,-./` (in order)
+      (keyCode > 218 && keyCode < 223); // [\]' (in order)
+    console.log(keyCode, isValid);
+    return isValid;
+  }
+
+  readyCommand(command: string) {
+    command = String(command).trim();
     this.cmdOffSet(-42);
     this.cmdPromptControls('clear');
     this.writeLine(command, this.logStyle(command));
@@ -68,10 +122,10 @@ export class WebCliComponent implements OnInit {
     this.executeCommand(cmd, tokens);
   }
 
-  getArgs(cmdLine) {
+  getArgs(cmdLine: string) {
     const tokenEx = /[^\s"]+|"[^"]*"/g;
     const quoteEx = /"/g;
-    const args = cmdLine.match(tokenEx);
+    const args = String(cmdLine).match(tokenEx);
 
     // Remove any quotes that may be in the args
     for (let i = 0; i < args.length; i++) {
@@ -106,18 +160,19 @@ export class WebCliComponent implements OnInit {
   IMG() {
     this.writeHTML(
       `<img height='200px' width='250px'
-       src="https://ip1gh35mejw4dpqjl4aya71p-wpengine.netdna-ssl.com/wp-content/uploads/2016/02/analytics-meme-sword-guy.png"/>`,
+       src="https://ip1gh35mejw4dpqjl4aya71p-wpengine.netdna-ssl.com/wp-content/uploads/2016/02/analytics-meme-sword-guy.png"/>`
     );
   }
 
   VIDEO() {
-    this.writeHTML(
-      `<iframe width="560" height="315" src="https://www.youtube.com/embed/hyC_3HHz13I" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`,
-    );
+    this.writeHTML(`<iframe width="200" height="200"
+     src="https://www.youtube-nocookie.com/embed/hyC_3HHz13I?rel=0&amp&controls=0&showinfo=0&modestbranding=1&showinfo=0&rel=0"
+    frameborder="0" allow="autoplay; encrypted-media"
+     ></iframe>`);
   }
 
   ENV() {
-    console.log(window);
+    console.log(this);
     this.cmdPromptOutputDivElement.nativeElement.innerHTML = this;
   }
 
@@ -135,7 +190,8 @@ export class WebCliComponent implements OnInit {
 
     this.cmdOffSet(adjustment);
 
-    this.cmdPromptElement.nativeElement.value = this.history[
+    // this.cmdPromptElement.nativeElement.value = this.history[
+    this.fakeInput.nativeElement.innerText = this.history[
       this.history.length - this.cmdOffSet()
     ];
   }
@@ -143,6 +199,7 @@ export class WebCliComponent implements OnInit {
   cmdPromptControls(action) {
     switch (action) {
       case 'focus':
+        console.log('why you no focus?');
         this.cmdPromptElement.nativeElement.focus();
         break;
       case 'clear':
